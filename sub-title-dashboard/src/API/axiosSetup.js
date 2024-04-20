f33fd644f709f91bf7fd83538e0ea2993ec1d294
andrew haliem <an.roooof@gmail.com>
2024-04-20 17:01:16 +0200

    first

--- sub-title-dashboard/src/API/axiosSetup.js
+++ sub-title-dashboard/src/API/axiosSetup.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const apisConfig = {
-    baseURL: "http://192.168.1.13:8080",
+    baseURL: "http://localhost:8080",
     timeout: 500000,
     headers: {
         "Access-Control-Allow-Origin": "*",
